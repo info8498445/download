b2c15697fad6228f1ee6adf88a1827b440353e1e
Sanskar Koserwal <sanskark008@gmail.com>
2025-12-26 01:34:51 +0530

    app added

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   useEffect(() => {
     // Automatically trigger download when page loads
     const link = document.createElement('a');
-    link.href = '/lvds.apk';
+    link.href = '/lvds-release.apk';
     link.download = 'LVDS.apk';
     document.body.appendChild(link);
     link.click();
